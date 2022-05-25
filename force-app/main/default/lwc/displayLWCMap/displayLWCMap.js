0618669fce818e21c7b9f34c3d1f1470550df917
t-gul <100842293+t-gul@users.noreply.github.com>
2022-05-25 17:00:05 +0300

    Venue Plan for Event (#82)
    * Venue Plan
    
    * Update Admin.profile-meta.xml
    
    Co-authored-by: Tuğrul GÜL <tgrgl@hotmail.com>

--- force-app/main/default/lwc/displayLWCMap/displayLWCMap.js
+++ force-app/main/default/lwc/displayLWCMap/displayLWCMap.js
@@ -1,28 +1,40 @@
 import { LightningElement , wire, track, api } from 'lwc';
-import { getRecord } from 'lightning/uiRecordApi';
+import { getRecord} from 'lightning/uiRecordApi';
 import { refreshApex } from '@salesforce/apex';
 export default class DisplayLWCMap extends LightningElement {
 
     @api recordId;
-    @track publicEvent;
+    @api objectApiName;
+    @track record;
+    @track fields;
     @track error;   //this holds errors
     @track mapMarkers = [];
     @track zoomLevel = 10;
+    
+    connectedCallback() {
+        this.fields =   [   this.objectApiName + '.Street__c',
+                            this.objectApiName + '.City__c',
+                            this.objectApiName + '.Country__c',
+                            this.objectApiName + '.Postal_Code__c'
+                        ];
+    }
 
-    @wire(getRecord, { recordId: '$recordId', fields: ['Public_Event__c.Street__c', 'Public_Event__c.City__c', 'Public_Event__c.Country__c', 'Public_Event__c.Postal_Code__c'] })
-    getpublicEvent({data,error}) {
+    @wire(getRecord,    { recordId: '$recordId', fields: '$fields'
+                        }
+    )
+    getmaprecords({data,error}) {
         if (data) {
-            this.publicEvent = data;
+            this.record = data;
             this.mapMarkers = [
                 {
                     location: {
-                        Street: this.publicEvent.fields.Street__c.value,
-                        City: this.publicEvent.fields.City__c.value,
-                        Country: this.publicEvent.fields.Country__c.value,
-                        PostalCode: this.publicEvent.fields.Postal_Code__c.value
+                        Street: this.record.fields.Street__c.value,
+                        City: this.record.fields.City__c.value,
+                        Country: this.record.fields.Country__c.value,
+                        PostalCode: this.record.fields.Postal_Code__c.value
                     },
                     icon: 'custom:custom11',
-                    title: this.publicEvent.fields.Name,
+                    title: this.record.fields.Name,
                 }                                    
             ];
             this.processRelatedObjects();
@@ -32,6 +44,6 @@ export default class DisplayLWCMap extends LightningElement {
     }
 
     processRelatedObjects() {
-        refreshApex(this.publicEvent);
+        refreshApex(this.record);
     }
 }
